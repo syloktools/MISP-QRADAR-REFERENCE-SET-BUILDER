a5f2a0a54314950b7d56a09f6fa06aeaf5cc4f4d
Robert Nixon <nixonrobert81@gmail.com>
2022-10-26 15:42:42 +0200

    Update master_qradar_misp_MD5.py

--- master_qradar_misp_MD5.py
+++ master_qradar_misp_MD5.py
@@ -12,7 +12,7 @@ data_pull = {"request": {"type": "md5", "category": "Payload delivery", "last": 
 url_push = 'https://<qradar_fqdn>/api/reference_data/sets/bulk_load/<reference_set_name>'
 headers_push = {'SEC': '<token>', 'Content-Type': 'application/json', 'Version': '9.0', 'Accept': 'application/json'}
 
-# Pull the list of attributes from TIA-REPO
+# Pull the list of attributes from a specific MISP instance
 r = requests.post(url_pull, headers=headers_pull, json=data_pull, verify=False)
 r.raise_for_status() #Raise exception on HTTP errors
 
